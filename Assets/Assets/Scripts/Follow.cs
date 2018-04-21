f74e629fedddaf569b8185317a0c7755f243a12d
damrem <damien.remars@gmail.com>
2018-04-21 16:16:35 +0200

    camera follows player

---
+++ Assets/Assets/Scripts/Follow.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Follow : MonoBehaviour {
+
+	public GameObject target;
+
+	void LateUpdate () {
+
+		Vector2 delta = target.transform.position - transform.position;
+
+		Rigidbody body = GetComponent<Rigidbody> ();
+		body.velocity = delta;
+	}
+}
